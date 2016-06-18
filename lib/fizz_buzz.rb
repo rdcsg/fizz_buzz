8415d778fba3da54a5f2df1786d05c572498fd92
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-06-18 15:13:24 +0200

    I want to test this after finding the end of statement that is required to make it work

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,16 +1,14 @@
 # lib/fizz_buzz.rb
 
 def fizz_buzz(number)
-  puts case a
-  when number % 3 == 0
-      "fizz!"
+  puts case number
+    when number % 3 === 0 then "fizz! "
 
-    when number % 5 == 0
-      "buzz!"
+    when number % 5 === 0 then "buzz! "
 
-    when number % 15 == 0
-      "fizz buzz!"
+    when number % 15 === 0 then "fizz buzz! "
 
     else
-      "hey, now you're not playing according to the rules! ;)"
+      number + " "
+      #"hey, that's no number, now you're not playing according to the rules! ;)"
 end
