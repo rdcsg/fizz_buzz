22ab989370fa040c5c6bcbcc938669ed7ad407e4
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-06-18 16:13:01 +0200

    final version

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -2,11 +2,11 @@
 
 def fizz_buzz(number)
   case
-  when (number % 15 == 0) then 'fizz buzz! '
+  when (number % 15 == 0) then 'fizz buzz!'
 
-  when (number % 3 == 0) then 'fizz! '
+  when (number % 3 == 0) then 'fizz!'
 
-  when (number % 5 == 0) then 'buzz! '
+  when (number % 5 == 0) then 'buzz!'
 
     else
       number #+ ' '
