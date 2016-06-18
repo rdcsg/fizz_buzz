721449ea674dea824d786749c28c0dcc02062f06
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-06-18 15:19:46 +0200

    switched all dbl quotes to single quotes

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -2,14 +2,14 @@
 
 def fizz_buzz(number)
   shout = case number
-    when number % 3 === 0 then "fizz! "
+  when number % 3 === 0 then 'fizz! '
 
-    when number % 5 === 0 then "buzz! "
+  when number % 5 === 0 then 'buzz! '
 
-    when number % 15 === 0 then "fizz buzz! "
+  when number % 15 === 0 then 'fizz buzz! '
 
     else
-      number + " "
+      number + ' '
       #"hey, that's no number, now you're not playing according to the rules! ;)"
 end
 
