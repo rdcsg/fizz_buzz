eeaea1bbe0d3e93fe0b0a1d35db07ab75841008a
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-06-18 14:55:49 +0200

    added first tests and first implementation to match the tests

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,5 +1,16 @@
 # lib/fizz_buzz.rb
 
 def fizz_buzz(number)
-  number
+  puts case a
+  when number % 3 == 0
+      "fizz!"
+
+    when number % 5 == 0
+      "buzz!"
+
+    when number % 15 == 0
+      "fizz buzz!"
+
+    else
+      "hey, now you're not playing according to the rules! ;)"
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -6,4 +6,20 @@ describe 'fizz_buzz' do
   it 'returns "1" if number = 1' do
       expect(fizz_buzz(1)).to eq 1
   end
+
+
+  it 'returns "fizz" if my_number is dividable by 3' do
+      expect(fizz_buzz(my_number)).to eq "fizz"
+  end
+
+
+  it 'returns "buzz" if my_number is dividable by 5' do
+      expect(fizz_buzz(my_number)).to eq "buzz"
+  end
+
+
+  it 'returns "fizz buzz" if my_number is dividable by 15' do
+      expect(fizz_buzz(my_number)).to eq "fizz buzz"
+  end
+
 end
