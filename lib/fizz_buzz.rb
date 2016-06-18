8229ab7b4cdc2a6c8d3d2a14bac8fe3f320146b8
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-06-18 15:18:06 +0200

    trying to put the printout at the end and to use the result assignation of which case it is inside the def statement

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,7 +1,7 @@
 # lib/fizz_buzz.rb
 
 def fizz_buzz(number)
-  puts case number
+  shout = case number
     when number % 3 === 0 then "fizz! "
 
     when number % 5 === 0 then "buzz! "
@@ -12,3 +12,5 @@ def fizz_buzz(number)
       number + " "
       #"hey, that's no number, now you're not playing according to the rules! ;)"
 end
+
+puts shout
